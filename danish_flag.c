542d1486933e07b910816f6a2c0338634e45d095
Harrison <harrison@theshoebridges.com>
2015-03-11 16:45:44 +1100

    danish_flag: add danish flag solution

---
+++ danish_flag.c
@@ -0,0 +1,20 @@
+// danish_flag: print the danish flag to stdout! , z5080306
+// HS1917
+// Harrison Shoebridge
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char * argv[]) {
+	printf("#####  ############\n");
+	printf("#####  ############\n");
+	printf("#####  ############\n");
+	
+	printf("\n");
+	
+	printf("#####  ############\n");
+	printf("#####  ############\n");
+	printf("#####  ############\n");
+
+	return 0;
+}
