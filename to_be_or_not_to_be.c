a48e4c8c411e9d41bff2530c7a68befba2b7c478
Harrison <harrison@theshoebridges.com>
2015-03-11 16:54:05 +1100

    to_be*_be.c: add solution, including s#^%ty variable names!

---
+++ to_be_or_not_to_be.c
@@ -0,0 +1,9 @@
+// print "To be, or not to be?"
+// Harrison Shoebridge, 11/3/15
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+	printf("To be, or not to be?\n");
+}
