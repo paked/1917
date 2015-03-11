c7e128649661b4ba98959134bfc0e30ac3c3ee35
Harrison <harrison@theshoebridges.com>
2015-03-11 17:43:21 +1100

    leap_year: solution!

---
+++ leap_year.c
@@ -0,0 +1,25 @@
+// Calculate if the number is a leap year!
+// Week x :)
+// Harrison Shoebridge!
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define START_OF_GREGORIAN_CALENDAR 1582
+
+int main(int argc, char * argv[]) {
+	int year;
+	printf("What year are you interested kind sir/madam!?\n");
+	scanf("%d", &year);
+	
+	assert(year > START_OF_GREGORIAN_CALENDAR);
+	
+	if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) { 
+		printf("%d is a leap year:)\n", year);
+	}else {
+		printf("%d is not a leap year :(\n", year);
+	}
+
+	return 0;
+}
