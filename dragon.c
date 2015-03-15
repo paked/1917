2fdd657b46dbcb43d3950f41256bba978b38714f
Harrison <harrison@theshoebridges.com>
2015-03-15 11:34:02 +1100

    dragon: add the "dragon"

---
+++ dragon.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+	printf("                   The Dragon by Arnold Riverstring   \n");
+	printf(" ____________________________________________________ \n");
+	printf("|                                                    |\n");
+	printf("|                                                    |\n");
+	printf("|                                                    |\n");
+	printf("|                                                    |\n");
+	printf("|                     -OO\"                           |\n");
+	printf("|                                                    |\n");
+	printf("|                                                    |\n");
+	printf("|                                                    |\n");
+	printf("|____________________________________________________|\n");
+	printf("This is an exquisite work by the late Arnold\n");
+	printf("Riverstring. It portrays a dragon in the distance, \n");
+	printf("relaxing in the sun underneath a palm tree while\n");
+	printf("drinking a lime-lemonade and munching on some\n");
+	printf("chocolate chips. One of the many beauties of this\n");
+	printf("work is it's use of distance and perspective, something\n");
+	printf("not many artists of this time period where doing.\n");
+	return 0;
+}
