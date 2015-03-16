bfa9e0f17d507c4f2eb9c6cf5fbeb4525719690b
Harrison <harrison@theshoebridges.com>
2015-03-17 09:53:42 +1100

    assignments/doomsday: add LICENSE and basic program

---
+++ assignments/doomsday/main.c
@@ -0,0 +1,15 @@
+// main.c
+// Find the day of the week of an arbitrary date using the 
+// using the doomsday method.
+// Written by Harrison Shoebridge on 17/3/15.
+// 
+// I didn't copy Richards code, so I'm not using Creative Commons.
+// MIT for the win!
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+	printf("Hello, world!\n");
+	return EXIT_SUCCESS;
+}
