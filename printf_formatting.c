e27199d43335d73c542e8ec0aebcc5de13e44e4c
Harrison <harrison@theshoebridges.com>
2015-03-17 14:56:48 +1100

    printf: some basic formatting tests

---
+++ printf_formatting.c
@@ -0,0 +1,10 @@
+// This is a test program to test the printing of doubles in C.
+// by Harrison Shoebridge 17/3/15
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+	double n = 1.53767;
+	printf("%.2f\n", n);
+	return EXIT_SUCCESS;
+}
