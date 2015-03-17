0525a25ad4335b87882291a75931e80b88342758
Harrison <harrison@theshoebridges.com>
2015-03-17 16:35:31 +1100

    main: add day of the week constants

--- assignments/doomsday/main.c
+++ assignments/doomsday/main.c
@@ -9,7 +9,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MONDAY 0
+#define TUEDAY 1
+#define WEDNESDAY 2
+#define THURSDAY 3
+#define FRIDAY 4
+#define SATURDAY 5
+#define SUNDAY 6
+
+int dayOfWeek(int doomsday, int leapYear, int month, int day);
+
 int main(int argc, char * argv[]) {
 	printf("Hello, world!\n");
 	return EXIT_SUCCESS;
+}
+
+int dayOfWeek(int doomsday, int leapYear, int month, int day) {
+	int dayOfWeek = MONDAY;
+
+	return dayOfweek
 }
