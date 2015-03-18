154e07acfa450c23faf2c9a2e1e7ae0856aba279
Harrison <harrison@theshoebridges.com>
2015-03-18 13:16:41 +1100

    sort: add basic file structure with scanf of 3 ints

---
+++ sort.c
@@ -0,0 +1,16 @@
+// sort.c
+// A program to sort three integers!
+// by Harrison Shoebridge on the 18/3/15
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int arg, char * argv[]) {
+    int x;
+    int y;
+    int z;
+
+    scanf("%d %d %d", &x, &y, &z);
+}
+
+
