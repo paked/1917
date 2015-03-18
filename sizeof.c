c86d8afd26a0ebc9c99dfeaea509f627ae86af63
Harrison <harrison@theshoebridges.com>
2015-03-18 12:47:00 +1100

    sizeof: add pointer size comparisons

--- sizeof.c
+++ sizeof.c
@@ -11,11 +11,25 @@ int main(int argc, char * argv[]) {
 	float f;
 	double d;
 
+	char *pc = &c;
+	int *pi = &i;
+	long *pl = &l;
+	float *pf = &f;
+	double *pd = &d;
+
 	printf("Finding the size of various C types... \n");
 	printf("size of char: %ld\n", sizeof(c));
 	printf("size of int: %ld\n", sizeof(i));
 	printf("size of long: %ld\n", sizeof(l));
 	printf("size of float: %ld\n", sizeof(f));
 	printf("size of double: %ld\n", sizeof(d));
+
+	printf("\nFinding the size to various pointers to C types...\n");
+	printf("size of pointer to char: %ld\n", sizeof(pc));
+	printf("size of pointer to int: %ld\n", sizeof(pi));
+	printf("size of pointer to long: %ld\n", sizeof(pl));
+	printf("size of pointer to float: %ld\n", sizeof(pf));
+	printf("size of pointer to double: %ld\n", sizeof(pd));
+	
 	return EXIT_SUCCESS;
 }
