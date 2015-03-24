1789847d2cb0cd5623b6564772a5cc7d178abe25
Harrison <harrison@theshoebridges.com>
2015-03-24 13:02:00 +1100

    +danish: print the danish flag with while loops

---
+++ danish.c
@@ -0,0 +1,31 @@
+// danish.c
+// Use for loops to print out the danish flags
+// by Harrison Shoebridge, 24/3/15
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void showDanish(void);
+
+int main(int argc, char * argv[]) {
+    showDanish();
+
+    return EXIT_SUCCESS;
+}
+
+void showDanish(void) {
+    int segments = 0;
+    while(segments < 2) {
+        int lines = 0;
+        while (lines < 2) {
+            printf("** *********\n");
+            lines += 1;
+        }
+
+        if (segments == 0) {
+            printf("\n");
+        }
+
+        segments += 1;
+    }   
+}
