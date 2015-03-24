7ea1ed7ce340b111ff7363932d22d6163f2281d8
Harrison <harrison@theshoebridges.com>
2015-03-24 12:49:38 +1100

    +wondrous: a function to print out the wondrous numbers

---
+++ wondrous.c
@@ -0,0 +1,38 @@
+// wondrous.c
+// Contains a function to print out the "wondrous"
+// seqence of numbers
+// by Harrison Shoebridge, 24/3/15
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int printWondrous(int start);
+
+int main(int argc, char * argv[]) {
+    assert(printWondrous(3) == 8);
+    assert(printWondrous(1) == 1);
+}
+
+int printWondrous(int start) {
+    int number = start;
+    int cycles = 1;
+
+    printf("%d ", number);
+    while(number != 1) {
+        if (number % 2 == 0) {
+            // the number is even
+            number = number / 2;
+        }else {
+            // odd
+            number = (number * 3) + 1;
+        }
+
+        printf("%d ", number);
+        cycles++;
+    }
+
+    printf("\n");
+
+    return cycles;
+}
