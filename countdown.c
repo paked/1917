10c9c91eef85dc7677f1fa8064fa2ea4adb3d6f6
Harrison <harrison@theshoebridges.com>
2015-04-16 16:16:50 +1000

    +countdown: count down from 100 with a for loop

---
+++ countdown.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    for(int i = 0; i < 100; i++) {
+        printf("%d\n", 100 - i);
+    }
+
+    return EXIT_SUCCESS;
+}
