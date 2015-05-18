5813e452f79819b950bc60ed0cc2b2270113fdc2
Harrison <harrison@theshoebridges.com>
2015-05-19 07:50:18 +1000

    +tictactoe: a basic tictactoe game

---
+++ tictactoe.c
@@ -0,0 +1,251 @@
+// tictactoe.c - for when you want to tic tac toe
+// by Harrison Shoebridge on the 18/5/15
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BOARD_SIZE 3
+
+#define EMPTY -1 
+#define PLAYER_ONE 0
+#define PLAYER_TWO 1
+
+#define GAME_WON 1
+#define GAME_TIED 2
+#define GAME_NOT_OVER 0
+
+typedef struct _game {
+    int map[BOARD_SIZE][BOARD_SIZE];
+    unsigned int roundCount;
+} Game;
+
+// TODO make "move" struct to represent a potential move
+
+// TODO split logic out of this main file
+
+// TODO make function names more specific to the "game"
+
+// TODO move contents of main into a seperate "run game" function
+
+// TODO allow symbol selection for characters
+
+// TODO create ADT
+
+void printSpot(int s);
+void printGame(Game g);
+Game newGame(void);
+int validMove(Game g, int x, int y);
+Game makeMove(Game g, int p, int x, int y);
+int end(Game g, int p);
+
+Game newGame(void) {
+    Game g;
+    int y = 0;
+
+    while (y < BOARD_SIZE) {
+        int x = 0;
+        while (x < BOARD_SIZE) {
+            g.map[y][x] = EMPTY;
+            x += 1;
+        }
+
+        y += 1;
+    }
+
+    g.roundCount = 0;
+
+    return g;
+}
+
+void printGame(Game g) {
+    printf("Round #%d\n", g.roundCount);
+
+    int y = 0;
+    
+    // TODO make formatting prettier, code and normal wise
+
+    printf("-------------\n");
+    
+    while (y < BOARD_SIZE) {
+        int x = 0;
+        printf("|");
+        while (x < BOARD_SIZE) {
+            printSpot(g.map[y][x]);
+            printf("|");
+            x += 1;
+        }
+
+        printf("\n");
+        printf("-------------\n");
+
+        y += 1;
+    }
+}
+
+void printSpot(int s) {
+    printf(" ");
+
+    if (s == EMPTY) {
+        printf(" ");
+    } else if (s == PLAYER_ONE) {
+        printf("x");
+    } else if (s == PLAYER_TWO) {
+        printf("o");
+    }
+
+    printf(" ");
+}
+
+int validMove(Game g, int x, int y) {
+    // boundary checks
+    if (x < 0 || y < 0 || x > BOARD_SIZE|| y > BOARD_SIZE) {
+        return 0;
+    }
+    
+    // move already made
+    if (g.map[y][x] != EMPTY) {
+        return 0;
+    }
+
+    return 1;
+}
+
+Game makeMove(Game g, int p, int x, int y) {
+    g.map[y][x] = p;
+
+    g.roundCount += 1;
+
+    return g;
+}
+
+int end(Game g, int p) {
+    if (g.roundCount == (BOARD_SIZE * BOARD_SIZE)) {
+        return GAME_TIED;
+    }
+
+    int x;
+    int y;
+    y = 0;
+
+    // horizontal check
+    while (y < BOARD_SIZE) {
+        x = 0;
+        while (x < BOARD_SIZE) {
+            if (g.map[y][x] != p) {
+                break;
+            }
+
+            x += 1;
+
+            if (x == BOARD_SIZE) {
+                return GAME_WON;
+            }
+        }
+
+        y += 1;
+    }
+    
+    // vertical check
+    y = 0;
+    x = 0;
+    while (x < BOARD_SIZE) {
+        y = 0;
+        while (y < BOARD_SIZE) {
+            if (g.map[y][x] != p) {
+                break;
+            }
+
+            y += 1;
+
+            if (y == BOARD_SIZE) {
+                return GAME_WON;
+            }
+        }
+        x += 1;
+    }
+
+    // left-top -> right-bottom diag check
+    // TODO neaten this up
+    y = 0;
+    x = 0;
+    while (x < BOARD_SIZE) {
+        if (g.map[x][x] != p) {
+            break;
+        }
+
+        x += 1;
+
+        if (x == BOARD_SIZE) {
+            return GAME_WON;
+        }
+    }
+
+    // right-top -> left-bottom diag check
+    // TODO neaten this up
+    int i = 0;
+    while (i < BOARD_SIZE) {
+        if (g.map[i][BOARD_SIZE- i - 1] != p) {
+            break;
+        }
+
+        i += 1;
+
+        if (i == BOARD_SIZE) {
+            return GAME_WON;
+        }
+    }
+
+    return GAME_NOT_OVER;
+}
+
+int main(int argc, char *argv[]) {
+    Game g = newGame();
+
+    while(1) {
+        printGame(g);
+
+        int p;
+        if (g.roundCount % 2 == 0) {
+            p = PLAYER_ONE;
+        } else {
+            p = PLAYER_TWO;
+        }
+
+
+        while (1) {
+            printf("Where would you like to place your player?\n");
+
+            int x, y;
+            scanf("%d %d", &x, &y);
+
+            x -= 1;
+            y -= 1;
+
+            printf("placing at grid cooords (%d %d)\n", x, y);
+            if (validMove(g, x, y)) {
+                g = makeMove(g, p, x, y);
+                break;
+            } 
+
+            printf("that is not a valid move, try again\n"); 
+        }
+
+        printf("\n");
+        
+        int result = end(g, p);
+
+        if (result != GAME_NOT_OVER) {
+            printGame(g);
+
+            if (result == GAME_WON) {
+                printf("\n\nplayer %d won!\n", p);
+            }else if (result == GAME_TIED) {
+                printf("\n\ngame over, tied!\n");
+            }
+
+            break;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
